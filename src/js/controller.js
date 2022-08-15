01e47028ab5bb655ad52bef11ff835a42d9276ce
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-08-15 11:43:55 +0100

    Installed NPM (Parcel/Sass) and added recipe API

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,37 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function() {
+  
+  try {
+
+    const response = await fetch(
+      `https://forkify-api.herokuapp.com/v2/recipes/5ed6604591c37cdc054bc886`
+    ); 
+    const data = await response.json();
+    
+    if(!response.ok) throw new Error(`${data.message} (${response.status})`);
+
+    let recipe = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.pubisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients
+    };
+    console.log(recipe);
+
+  } catch(err) {
+
+    alert(err);
+
+  }
+
+};
+
+showRecipe();
